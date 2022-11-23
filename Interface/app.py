8a12368341544112292bedad6d7eff2d669d856c
VICTOR ROZA SOUZA <enricofermi2019@gmail.com>
2022-11-23 17:30:04 -0400

    adicionando botoes/cores e funcinalidade matematica

--- Interface/app.py
+++ Interface/app.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QGridLayout, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QGridLayout, QLineEdit, QSizePolicy
 
 
 class Calculadora(QMainWindow):
@@ -12,9 +12,64 @@ class Calculadora(QMainWindow):
 
         self.display = QLineEdit()
         self.grid.addWidget(self.display, 0, 0, 1, 5)
-        self.setDisabled(True)
+        self.display.setDisabled(True)
+        self.display.setStyleSheet(
+            '* {background: white; color: #000; font-size: 30px}'
+        )
+        self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
+        self.add_botao(QPushButton('7'), 1, 0, 1, 1)
+        self.add_botao(QPushButton('8'), 1, 1, 1, 1)
+        self.add_botao(QPushButton('9'), 1, 2, 1, 1)
+        self.add_botao(QPushButton('+'), 1, 3, 1, 1)
+        self.add_botao(QPushButton('C'), 1, 4, 1, 1, lambda: self.display.setText(''),
+                       'background: red; color: #fff; font-weight: 700;')
+
+        self.add_botao(QPushButton('4'), 2, 0, 1, 1)
+        self.add_botao(QPushButton('5'), 2, 1, 1, 1)
+        self.add_botao(QPushButton('6'), 2, 2, 1, 1)
+        self.add_botao(QPushButton('-'), 2, 3, 1, 1)
+        self.add_botao(QPushButton('<-'), 2, 4, 1, 1, lambda: self.display.setText(self.display.text()[:-1]),
+                       'background: orange; color: #fff; font-weight: 700;')
+
+        self.add_botao(QPushButton('1'), 3, 0, 1, 1)
+        self.add_botao(QPushButton('2'), 3, 1, 1, 1)
+        self.add_botao(QPushButton('3'), 3, 2, 1, 1)
+        self.add_botao(QPushButton('/'), 3, 3, 1, 1)
+        self.add_botao(QPushButton(''), 3, 4, 1, 1)
+
+        self.add_botao(QPushButton('.'), 4, 0, 1, 1)
+        self.add_botao(QPushButton('0'), 4, 1, 1, 1)
+        self.add_botao(QPushButton(''), 4, 2, 1, 1)
+        self.add_botao(QPushButton('*'), 4, 3, 1, 1)
+        self.add_botao(QPushButton('='), 4, 4, 1, 1, self.igual,
+                       'background: navy; color: #fff; font-weight: 700;')
 
         self.setCentralWidget(self.cw)
+
+    def add_botao(self, botao, row, col, rowspan, colspan, func=None, style=None):
+        self.grid.addWidget(botao, row, col, rowspan, colspan)
+
+        if not func:
+            botao.clicked.connect(
+                lambda: self.display.setText(
+                    self.display.text() + botao.text()
+                )
+            )
+        else:
+            botao.clicked.connect(func)
+
+        if style:
+            botao.setStyleSheet(style)
+
+        botao.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
+    def igual(self):
+        try:
+            self.display.setText(str(eval(self.display.text())))
+        except Exception as e:
+            self.display.setText('Conta inválida')
+
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
